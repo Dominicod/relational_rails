b47c4572855d78768f0aecd2af9bb38d0c4c63de
dominicod <dominicodonnell99@gmail.com>
2022-08-22 19:26:01 -0500

    feat: create routes for vehicles and dealerships

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/dealerships', to: 'dealerships#index'
+  get '/dealerships/:id', to: 'dealerships#show'
+  get '/dealerships/:id/vehicles', to: 'dealerships#parent_children_index'
+
+  get '/vehicles', to: 'vehicles#index'
+  get '/vehicles/:id', to: 'vehicles#show'
 end
