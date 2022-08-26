8402673402c862e0b1f2a58e5036e72d3cdef9fc
dominicod <dominicodonnell99@gmail.com>
2022-08-26 16:06:53 -0500

    add private methods to Controllers for security

--- app/controllers/dealership_vehicles_controller.rb
+++ app/controllers/dealership_vehicles_controller.rb
@@ -1,23 +1,28 @@
 class DealershipVehiclesController < ApplicationController
   def new
-    @dealer = index
+    @dealer = find_dealer
   end
 
   def index
-    @dealership = Dealership.find(params[:id])
+    @dealership = find_dealer
   end
 
   def create
-    dealer = Dealership.find(params[:id])
+    dealer = find_dealer
     dealer.vehicles.create!(name: v_params[:name], cylinder_count: v_params[:cylinder_count],
                        horsepower: v_params[:horsepower], torque: v_params[:torque],
                        luxury_model: v_params[:luxury_model])
     redirect_to "/dealerships/#{dealer.id}/vehicles"
   end
 
+  private
   def v_params
     {name: params[:vehicle][:name], cylinder_count: params[:vehicle][:cylinder_count],
      horsepower: params[:vehicle][:horsepower], torque: params[:vehicle][:torque],
      luxury_model: params[:vehicle][:luxury_model] ? true : false}
+  end
+
+  def find_dealer
+    Dealership.find(params[:id])
   end
 end
--- app/controllers/dealerships_controller.rb
+++ app/controllers/dealerships_controller.rb
@@ -13,23 +13,28 @@ class DealershipsController < ApplicationController
   end
 
   def show
-    @dealership = Dealership.find(params[:id])
+    @dealership = find_dealer
   end
 
   def edit
-    @dealer = show
+    @dealer = find_dealer
   end
 
   def update
-    task = show
+    task = find_dealer
     task.update(name: d_params[:name], vehicle_lot_size: d_params[:lot_size],
                 service_center: d_params[:service_center], car_wash: d_params[:car_wash])
     redirect_to "/dealerships/#{task.id}"
   end
 
+  private
   def d_params
     {name: params[:dealer][:name], lot_size: params[:dealer][:lot_size],
      service_center: params[:dealer][:service_center] ? true : false,
      car_wash: params[:dealer][:car_wash] ? true : false}
+  end
+
+  def find_dealer
+    Dealership.find(params[:id])
   end
 end
